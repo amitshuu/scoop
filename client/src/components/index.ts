89919c6aae300b432e00cd48d1806039e78cb1d1
Amit <amitshuu@gmail.com>
2023-01-13 13:46:32 +0200

    Imports && Exports

---
+++ client/src/components/index.ts
@@ -0,0 +1,6 @@
+import ComposePost from './ComposePost';
+import Logo from './Logo';
+import Sidebar from './Sidebar';
+import SidebarLinks from './SidebarLinks';
+
+export { Logo, Sidebar, SidebarLinks, ComposePost };
