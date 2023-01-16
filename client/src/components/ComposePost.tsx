c6fbed241c3a2c1eb2c7bfc2f6dff986247623bf
Amit <amitshuu@gmail.com>
2023-01-16 20:22:19 +0200

    Css improvements

--- client/src/components/ComposePost.tsx
+++ client/src/components/ComposePost.tsx
@@ -1,27 +1,37 @@
 import styled from 'styled-components';
+import amit from '../assets/amit.jpg';
 import ImageIcon from '@mui/icons-material/Image';
 import VideoCameraBackIcon from '@mui/icons-material/VideoCameraBack';
+import { mobile } from '../utils/responsive';
 const ComposePost = () => {
   return (
     <Wrapper>
-      <PostCompositionContainer>
+      <HomeTitleContainer>
+        <HomeTitle>Home</HomeTitle>
+        <SortBySpan>Sort by:</SortBySpan>
+      </HomeTitleContainer>
+      <Container>
         <TopBarContainer>
-          <Placeholder>Compose new post</Placeholder>
+          <AvatarImage src={amit} />
+          <PostComposingDiv>
+            <Placeholder>What do you want to share Amit?</Placeholder>
+          </PostComposingDiv>
         </TopBarContainer>
         <BottomBarContainer>
-          <PostTypesContainer>
-            <PostType>
-              <ImageIcon />
-              <Text>Add Image</Text>
-            </PostType>
-            <PostType>
-              <VideoCameraBackIcon />
-              <Text>Add Video</Text>
-            </PostType>
-          </PostTypesContainer>
-          <PublishButton>Publish</PublishButton>
+          <PostType>
+            <ImageIcon
+              style={{
+                color: 'var(--clr-green-primary)',
+              }}
+            />
+            <Text>Add Image</Text>
+          </PostType>
+          <PostType>
+            <VideoCameraBackIcon style={{ color: 'var(--clr-like-red)' }} />
+            <Text>Add Video</Text>
+          </PostType>
         </BottomBarContainer>
-      </PostCompositionContainer>
+      </Container>
     </Wrapper>
   );
 };
@@ -30,30 +40,43 @@ export default ComposePost;
 
 const Wrapper = styled.div`
   display: flex;
-  width: 100%;
+  flex-direction: column;
+  align-items: center;
   margin-top: 3rem;
+  width: 100%;
+  ${mobile({ width: '90%' })}
 `;
 
-const PostCompositionContainer = styled.div`
+const HomeTitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 75%;
+  max-width: 650px;
+  ${mobile({ display: 'none' })}
+`;
+const HomeTitle = styled.h1`
+  font-weight: 600;
+`;
+
+const SortBySpan = styled.span``;
+
+const Container = styled.div`
   display: flex;
   flex-direction: column;
-  cursor: pointer;
-  width: 100%;
-  background-color: rgb(217, 217, 217, 20%);
-  border-radius: 10px;
-  height: 160px;
-  transition: all 0.2s;
-
-  &:hover {
-    background-color: rgb(217, 217, 217, 40%);
-  }
+  width: 75%;
+  max-width: 650px;
+  margin-top: 2rem;
+  background-color: white;
+  border-radius: 16px;
 `;
 
 const TopBarContainer = styled.div`
   display: flex;
-  height: 50%;
+  max-height: 80px;
   align-items: center;
-  padding: 2rem 3rem;
+  justify-content: space-evenly;
+  padding: 2rem 0rem;
   width: 100%;
   transition: all 0.2s;
 `;
@@ -61,50 +84,54 @@ const TopBarContainer = styled.div`
 const BottomBarContainer = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
-  padding: 2rem;
-  height: 50%;
+  padding: 1.5rem;
+  max-height: 80px;
   width: 100%;
   border-top: 2px solid rgb(0, 0, 0, 5%);
 `;
 
-const PostTypesContainer = styled.div`
+const PostComposingDiv = styled.div`
+  background-color: #f0f2f5;
   display: flex;
-  width: 50%;
+  align-items: center;
+  cursor: pointer;
+  border-radius: 25px;
+  height: 50px;
+  width: 80%;
+  transition: all 0.2s;
+  &:hover {
+    background-color: #d5d7db9e;
+  }
+`;
+
+const AvatarImage = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+`;
+
+const Placeholder = styled.span`
+  color: rgba(0, 0, 0, 60%);
+  margin-left: 2rem;
+  letter-spacing: 0.5px;
 `;
 
 const PostType = styled.div`
   display: flex;
   cursor: pointer;
   transition: all 0.2s;
-  padding: 1rem;
+  padding: 0rem 1rem;
+  height: 50px;
+  align-items: center;
   border-radius: 10px;
   &:hover {
-    background-color: rgb(217, 217, 217, 30%);
+    border-radius: 1rem;
+    background-color: #d5d7db9e;
   }
 `;
 
 const Text = styled.p`
   margin-left: 10px;
-`;
-
-const PublishButton = styled.button`
-  display: flex;
-  background-color: var(--clr-green-primary);
-  justify-content: center;
-  align-items: center;
-  border-radius: 10px;
-  border: 1px solid black;
-  width: 125px;
-  height: 35px;
-  cursor: pointer;
-  font-weight: 550;
-  transition: all 0.2s;
-  &:hover {
-    background-color: var(--clr-green-primary-hover);
-  }
-`;
-
-const Placeholder = styled.p`
-  color: rgb(0, 0, 0, 50%);
+  color: rgba(0, 0, 0, 60%);
+  font-weight: 500;
 `;
