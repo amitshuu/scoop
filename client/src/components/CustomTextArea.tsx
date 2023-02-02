8047e0a49541dfce74e72f5bb3e72b64860cc9a7
Amit <amitshuu@gmail.com>
2023-02-02 19:28:20 +0200

    Code refactor

--- client/src/components/CustomTextArea.tsx
+++ client/src/components/CustomTextArea.tsx
@@ -1,61 +1,47 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
-
+import TextareaAutosize from 'react-textarea-autosize';
 interface Props {
-  value: string;
-  onChange: (value: string) => void;
-  placeholder: string;
-  withBorder?: boolean;
-  withOverflow?: boolean;
-  maxHeight?: string;
+  value?: string;
+  onChange?: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  imageSrc?: string;
 }
 
-const CustomTextArea = ({
-  value,
-  onChange,
-  placeholder,
-  withOverflow = false,
-  maxHeight = '350px',
-}: Props) => {
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
-
-  useEffect(() => {
-    if (textareaRef.current) {
-      textareaRef.current.style.height = '';
-      textareaRef.current.style.height =
-        textareaRef.current.scrollHeight + 'px';
-    }
-  }, [value]);
-
+const CustomTextArea = ({ value, onChange, imageSrc }: Props) => {
   return (
-    <TextArea
-      ref={textareaRef}
-      value={value}
-      placeholder={placeholder}
-      onChange={(e) => onChange(e.target.value)}
-      withOverflow={withOverflow}
-      maxHeight={maxHeight}
-    />
+    <Wrapper>
+      <TextArea
+        value={value}
+        onChange={onChange}
+        placeholder='What do you want to share Amit?'
+      />
+      {imageSrc && <PostImage src={imageSrc} alt='' />}
+    </Wrapper>
   );
 };
 export default CustomTextArea;
 
-const TextArea = styled.textarea<{
-  withOverflow: boolean;
-  maxHeight: string;
-}>`
+const Wrapper = styled.div`
   display: flex;
-  resize: none;
-  padding: 10px;
-  border-radius: 16px;
-  outline: none;
+  flex-wrap: wrap;
+  height: 100%;
   width: 100%;
   border: none;
-  height: 70%;
-  max-height: ${({ maxHeight }) => (maxHeight ? maxHeight : 'none')};
-  color: #050505;
-  overflow-y: ${({ withOverflow }) => (withOverflow ? 'auto' : 'hidden')};
-  font-weight: 400;
+  padding: 0rem 1.5rem 1.5rem;
+  overflow-y: auto;
+`;
+
+const TextArea = styled(TextareaAutosize)`
+  border: none;
+  outline: none;
+  resize: none;
+  width: 100%;
+  margin-bottom: 2rem;
   font-size: 15px;
   font-family: inherit;
+  overflow-y: hidden;
+`;
+
+const PostImage = styled.img`
+  width: 100%;
 `;
