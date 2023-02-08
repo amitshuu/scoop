85d32bfea8fd61ed2224818a688595363434db66
Amit <amitshuu@gmail.com>
2023-02-08 18:44:18 +0200

    Implemented user routes && express.json middleware

--- server/server.ts
+++ server/server.ts
@@ -3,9 +3,13 @@ import morgan from 'morgan';
 import 'dotenv/config';
 import { connectDB } from './database/connect';
 import mongoose from 'mongoose';
+import userRouter from './routes/userRouter';
 
 const app = express();
+app.use(express.json());
 app.use(morgan('dev'));
+
+app.use('/api/auth', userRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
